7299305ab1f7d6a0e753ac38b00077ab97160668
sik9252 <sik9252@naver.com>
2023-07-30 17:09:46 +0900

    fix: UI 속성 추가

--- src/components/InputUI/index.tsx
+++ src/components/InputUI/index.tsx
@@ -1,13 +1,17 @@
-import { Input } from '@chakra-ui/react';
+import { Input, InputGroup, InputRightElement, Button } from '@chakra-ui/react';
 
 interface InputProps {
+  width?: string;
   placeholder: string;
+  show?: boolean;
+  onClick?: () => void;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-function InputUI({ placeholder }: InputProps) {
+export default function InputUI({ width, placeholder, onChange }: InputProps) {
   return (
     <Input
-      width="91%"
+      width={width ? width : '91%'}
       placeholder={placeholder}
       size="lg"
       variant="outline"
@@ -16,8 +20,36 @@ function InputUI({ placeholder }: InputProps) {
       _focus={{
         boxShadow: 'none',
       }}
+      backgroundColor="#ffffff"
+      onChange={onChange}
     />
   );
 }
 
-export default InputUI;
+export const PasswordInputUI = ({ width, placeholder, show, onClick, onChange }: InputProps) => {
+  return (
+    <InputGroup size="md">
+      <Input
+        width={width}
+        type={show ? 'text' : 'password'}
+        placeholder={placeholder}
+        size="lg"
+        variant="outline"
+        borderColor="gray.300"
+        focusBorderColor="gray.300"
+        _focus={{
+          boxShadow: 'none',
+        }}
+        backgroundColor="#ffffff"
+        mt="10px"
+        onChange={onChange}
+        autoComplete="off"
+      />
+      <InputRightElement width="4.5rem">
+        <Button mt="1.75rem" h="1.75rem" size="sm" onClick={onClick}>
+          {show ? 'Hide' : 'Show'}
+        </Button>
+      </InputRightElement>
+    </InputGroup>
+  );
+};
