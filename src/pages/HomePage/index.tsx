4686af734264532006b0b7aa5aca69ed027d4999
sik9252 <sik9252@naver.com>
2023-08-09 08:10:43 +0900

    feat: 관리자 메인페이지 로고 삽입

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,5 +1,12 @@
+import SoonMap_Vertical from '../../assets/soonmap_vertical.png';
+import { HomePageContainer, Image } from './style';
+
 function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <HomePageContainer>
+      <Image src={SoonMap_Vertical} alt="" />
+    </HomePageContainer>
+  );
 }
 
 export default HomePage;
---
+++ src/pages/HomePage/style.ts
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+
+export const HomePageContainer = styled.div`
+  margin: auto;
+`;
+
+export const Image = styled.img`
+  width: 50%;
+  margin: 0 auto;
+`;
