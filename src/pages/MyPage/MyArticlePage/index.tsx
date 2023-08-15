83dae5f9fe47b8fec0011d16c92f4e313238499e
sik9252 <sik9252@naver.com>
2023-08-15 21:04:20 +0900

    fix: 내가 작성한 글 페이지 -> 마이페이지 탭으로 분리

--- src/pages/MyPage/MyArticlePage/index.tsx
+++ src/pages/MyPage/MyArticlePage/index.tsx
@@ -1,5 +1,47 @@
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react';
+import RightContainer from '../../../components/layout/RightContainer';
+import Info from './Info';
+import Notice from './Notice';
+
 function MyArticlePage() {
-  return <div>MyArticlePage</div>;
+  const ACCOUNT_MENU = [
+    {
+      id: 1,
+      tabName: '작성한 공지사항',
+    },
+    {
+      id: 2,
+      tabName: '작성한 정보',
+    },
+  ];
+
+  const ACCOUNT_SCREEN = [
+    {
+      id: 1,
+      screen: <Notice />,
+    },
+    {
+      id: 2,
+      screen: <Info />,
+    },
+  ];
+
+  return (
+    <RightContainer title={'내가 작성한 글 관리'}>
+      <Tabs variant="enclosed">
+        <TabList>
+          {ACCOUNT_MENU.map((menu) => (
+            <Tab key={menu.id}>{menu.tabName}</Tab>
+          ))}
+        </TabList>
+        <TabPanels>
+          {ACCOUNT_SCREEN.map((screen) => (
+            <TabPanel key={screen.id}>{screen.screen}</TabPanel>
+          ))}
+        </TabPanels>
+      </Tabs>
+    </RightContainer>
+  );
 }
 
 export default MyArticlePage;
---
+++ src/pages/MyPage/MyArticlePage/style.ts
@@ -0,0 +1,74 @@
+import styled from '@emotion/styled';
+
+export const MyArticleSection = styled.div`
+  display: flex;
+`;
+
+export const MyArticleListSection = styled.div`
+  width: 60%;
+
+  @media only screen and (min-width: 1700px) {
+    width: 50%;
+  }
+
+  @media only screen and (min-width: 1995px) {
+    width: 40%;
+  }
+
+  @media only screen and (min-width: 2421px) {
+    width: 30%;
+  }
+`;
+
+export const SearchSection = styled.div`
+  display: flex;
+  align-items: center;
+
+  & > button {
+    height: 49px;
+    margin-bottom: 9px;
+    margin-left: 5px;
+  }
+`;
+
+export const CategoryFilterSection = styled.div`
+  width: 100%;
+  overflow-x: scroll;
+  display: flex;
+  align-items: center;
+  margin-bottom: 10px;
+`;
+
+export const MyArticlePreviewSection = styled.div`
+  width: 50%;
+  height: calc(100vh - 170px);
+  margin-left: 10px;
+  box-shadow: 0px 3px 10px #00000029;
+  border-radius: 6px;
+  padding: 20px;
+  overflow-y: scroll;
+
+  @media only screen and (min-width: 1700px) {
+    width: 60%;
+  }
+
+  @media only screen and (min-width: 1995px) {
+    width: 60%;
+  }
+
+  @media only screen and (min-width: 2421px) {
+    width: 70%;
+  }
+`;
+
+export const PreviewTitle = styled.div`
+  font-size: 20px;
+  font-weight: 700;
+`;
+
+export const PreviewMyArticle = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 15px;
+  font-size: 15px;
+`;
