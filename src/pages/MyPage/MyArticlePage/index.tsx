5f2e4ea85a49dccaeb51f8d691e13885f7344464
sik9252 <sik9252@naver.com>
2023-08-15 21:27:03 +0900

    feat: 공지사항 탭 노출 제어로직 추가

--- src/pages/MyPage/MyArticlePage/index.tsx
+++ src/pages/MyPage/MyArticlePage/index.tsx
@@ -2,9 +2,24 @@ import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react';
 import RightContainer from '../../../components/layout/RightContainer';
 import Info from './Info';
 import Notice from './Notice';
+import { useSelectedArticleAtom } from '../../../store/articleAtom';
+import { useEffect } from 'react';
 
 function MyArticlePage() {
-  const ACCOUNT_MENU = [
+  const { resetAtom } = useSelectedArticleAtom();
+
+  useEffect(() => {
+    resetAtom();
+  }, []);
+
+  const ACCOUNT_MENU_DEFAULT = [
+    {
+      id: 1,
+      tabName: '작성한 정보',
+    },
+  ];
+
+  const ACCOUNT_MENU_ADMIN = [
     {
       id: 1,
       tabName: '작성한 공지사항',
@@ -12,6 +27,13 @@ function MyArticlePage() {
     {
       id: 2,
       tabName: '작성한 정보',
+    },
+  ];
+
+  const ACCOUNT_SCREEN_DEFAULT = [
+    {
+      id: 1,
+      screen: <Info />,
     },
   ];
 
@@ -26,33 +48,35 @@ function MyArticlePage() {
     },
   ];
 
-  const ACCOUNT_SCREEN_DEFAULT = [
-    {
-      id: 1,
-      screen: <Info />,
-    },
-  ];
-
   return (
     <RightContainer title={'내가 작성한 글 관리'}>
       <Tabs variant="enclosed">
-        <TabList>
-          {ACCOUNT_MENU.map((menu) => (
-            <Tab key={menu.id}>{menu.tabName}</Tab>
-          ))}
-        </TabList>
         {localStorage.getItem('auth') === 'one' ? (
-          <TabPanels>
-            {ACCOUNT_SCREEN_ADMIN.map((screen) => (
-              <TabPanel key={screen.id}>{screen.screen}</TabPanel>
-            ))}
-          </TabPanels>
+          <>
+            <TabList>
+              {ACCOUNT_MENU_ADMIN.map((menu) => (
+                <Tab key={menu.id}>{menu.tabName}</Tab>
+              ))}
+            </TabList>
+            <TabPanels>
+              {ACCOUNT_SCREEN_ADMIN.map((screen) => (
+                <TabPanel key={screen.id}>{screen.screen}</TabPanel>
+              ))}
+            </TabPanels>
+          </>
         ) : (
-          <TabPanels>
-            {ACCOUNT_SCREEN_DEFAULT.map((screen) => (
-              <TabPanel key={screen.id}>{screen.screen}</TabPanel>
-            ))}
-          </TabPanels>
+          <>
+            <TabList>
+              {ACCOUNT_MENU_DEFAULT.map((menu) => (
+                <Tab key={menu.id}>{menu.tabName}</Tab>
+              ))}
+            </TabList>
+            <TabPanels>
+              {ACCOUNT_SCREEN_DEFAULT.map((screen) => (
+                <TabPanel key={screen.id}>{screen.screen}</TabPanel>
+              ))}
+            </TabPanels>
+          </>
         )}
       </Tabs>
     </RightContainer>
