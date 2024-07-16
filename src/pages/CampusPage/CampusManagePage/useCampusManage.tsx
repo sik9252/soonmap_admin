e0e7ab8f52e126dba91a5f3b61bb9bc78985735a
sik9252 <sik9252@naver.com>
2024-07-16 20:07:53 +0900

    refactor: 사용하지 않는 코드 제거

--- src/pages/CampusPage/CampusManagePage/useCampusManage.tsx
+++ src/pages/CampusPage/CampusManagePage/useCampusManage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { useSelectedBuildingAtom } from '../../../store/buildingAtom';
 import { useGetBuildingRequest } from '../../../api-requests/Building';
